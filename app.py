4926ce78d49a56dd875f79e5a92385383616df1e
Leandru Martin <leandrumartin@gmail.com>
2026-06-29 15:03:30 -0500

    Show model cheat sheet, use multimodal input, and render reasoning

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import gradio as gr
 from foundry import FoundryManager
 
-models = ["qwen2.5-0.5b", "qwen2.5-coder-0.5b", "qwen2.5-1.5b", "qwen2.5-coder-1.5b", "qwen2.5-7b", "qwen2.5-coder-7b"]
+models = [
+    "qwen2.5-0.5b", "qwen2.5-coder-1.5b", "qwen2.5-coder-7b", # chat, tools. All run on NPU
+    "phi-3.5-mini", # chat. All run on NPU
+    "qwen3.5-2b", "qwen3.5-4b", # vision-language-chat, tools, reasoning. All run on GPU
+]
 default_model = models[0]
 retain_loaded_models = False
 
@@ -25,7 +29,7 @@ def main():
                 interactive=False,
             )
 
-            input_textbox = gr.Textbox(
+            input_textbox = gr.MultimodalTextbox(
                 interactive=True,
                 submit_btn=True,
                 stop_btn=True,
@@ -33,6 +37,22 @@ def main():
             )
 
             return loaded_models_list, input_textbox
+        
+        gr.Markdown(
+            """
+            # Model Cheat Sheet
+
+            * Chat, tools (NPU):
+                * qwen2.5-0.5b
+                * qwen2.5-coder-1.5b
+                * qwen2.5-coder-7b
+            * Chat (NPU)
+                * phi-3.5-mini (4b parameters)
+            * Vision, chat, tools, reasoning (GPU)    
+                * qwen3.5-2b
+                * qwen3.5-4b
+            """,
+        )
 
         retain_checkbox = gr.Checkbox(
             label="Retain Loaded Models",
@@ -43,10 +63,17 @@ def main():
         loaded_models_list, chat_input = load_model(default_model, retain_loaded_models)
         model_select.change(load_model, inputs=[model_select, retain_checkbox], outputs=[loaded_models_list, chat_input])
 
+        chatbot = gr.Chatbot(
+            reasoning_tags=[("<think>", "</think>")],
+            autoscroll=False,
+        )
+
         gr.ChatInterface(
+            chatbot=chatbot,
             textbox=chat_input,
             save_history=True,
             fn=manager.get_model_response,
+            autoscroll=False,
         )
 
     full_interface.launch()
